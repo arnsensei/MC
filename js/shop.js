84d246044ed4cf40084a9a51944838c0e358c1aa
arnsensei <arnsenpaii@gmail.com>
2025-06-23 18:54:04 +0700

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,23 +1,23 @@
 function Vip1() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/arnsensei";
 }
 
 function Vip2() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/arnsensei";
 }
 
 function Vip3() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/arnsensei";
 }
 
 function Vip4() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/arnsensei";
 }
 
 function Vip5() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/arnsensei";
 }
 
 function Vip6() {
-    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+    window.location.href = "https://github.com/arnsensei";
 }
